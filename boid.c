009617440bb5720c0580e2814dadbe26520c8199
Colin Steifel <colin@steifel.net>
2012-04-27 18:11:24 -0400

    Fixed stupid reallocation on every call

--- boid.c
+++ boid.c
@@ -5,10 +5,11 @@ int boidInsert(boidContainer * container, boid * insert){
 	//Check for need to extend container
 	if(container->size == container->alloc){
 		container->alloc += CONTAINEREXTEND;
-	}
-	if((container->boidArr = (boid *) realloc(container->boidArr, container->alloc * sizeof(boid))) == NULL){
-		fprintf(stderr, "Error extending boid container, quitting...\n");
-		exit(1);
+
+		if((container->boidArr = (boid *) realloc(container->boidArr, container->alloc * sizeof(boid))) == NULL){
+			fprintf(stderr, "Error extending boid container, quitting...\n");
+			exit(1);
+		}
 	}
 	
 	container->boidArr[container->size] = *insert;
